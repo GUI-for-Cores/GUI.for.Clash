61bdd5d9819a7b66874ff0b0d83a8cf460293e48
Ayideyia <Ayideyia@users.noreply.github.com>
2023-11-27 00:34:55 +0800

    Update AboutView

--- frontend/src/utils/env.ts
+++ frontend/src/utils/env.ts
@@ -3,3 +3,7 @@ export const APP_TITLE = import.meta.env.VITE_APP_TITLE
 export const APP_VERSION = import.meta.env.VITE_APP_VERSION
 
 export const PROJECT_URL = import.meta.env.VITE_APP_PROJECT_URL
+
+export const TG_GROUP = import.meta.env.VITE_APP_TG_GROUP
+
+export const TG_CHANNEL = import.meta.env.VITE_APP_TG_CHANNEL
