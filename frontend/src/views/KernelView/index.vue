9c9eec168aabf30926c9f3d08483d016de1d7f87
onejibang <onejibang@users.noreply.github.com>
2024-01-31 16:45:21 +0800

    refactor: KernelView

---
+++ frontend/src/views/KernelView/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import StableBranch from './StableBranch.vue'
+import AlphaBranch from './AlphaBranch.vue'
+import SwitchBranch from './SwitchBranch.vue'
+</script>
+
+<template>
+  <div>
+    <StableBranch />
+    <AlphaBranch />
+    <SwitchBranch />
+  </div>
+</template>
+
+<style lang="less" scoped></style>
