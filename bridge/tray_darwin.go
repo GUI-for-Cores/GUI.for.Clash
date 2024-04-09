5569aece00de854e777e92254ee48f72e4f7341a
Ayideyia <Ayideyia@users.noreply.github.com>
2024-04-09 10:46:40 -0700

    chore: Remove compile flags

--- bridge/tray_darwin.go
+++ bridge/tray_darwin.go
@@ -1,3 +1,38 @@
 //go:build darwin
 
 package bridge
+
+import (
+	"embed"
+	"os/exec"
+
+	"github.com/wailsapp/wails/v2/pkg/runtime"
+)
+
+func CreateTray(a *App, icon []byte, fs embed.FS) {
+}
+
+func (a *App) UpdateTray(tray TrayContent) {
+}
+
+func (a *App) UpdateTrayMenus(menus []MenuItem) {
+}
+
+func (a *App) ExitApp() {
+	runtime.Quit(a.Ctx)
+}
+
+func (a *App) RestartApp() FlagResult {
+	exePath := Env.BasePath + "\\" + Env.AppName
+
+	cmd := exec.Command(exePath)
+
+	err := cmd.Start()
+	if err != nil {
+		return FlagResult{false, err.Error()}
+	}
+
+	a.ExitApp()
+
+	return FlagResult{true, "Success"}
+}
