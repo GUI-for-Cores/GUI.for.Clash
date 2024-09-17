ad53e9763a69cb45dc4ef8767338d50182f2f170
Ayideyia <150233177+Ayideyia@users.noreply.github.com>
2024-09-17 21:52:57 +0800

    feat: Adapt to the latest core

--- frontend/src/stores/kernelApi.ts
+++ frontend/src/stores/kernelApi.ts
@@ -101,7 +101,7 @@ export const useKernelApiStore = defineStore('kernelApi', () => {
 
     const onOut = async (out: string, pid: number) => {
       logsStore.recordKernelLog(out)
-      if (out.toLowerCase().includes('start initial compatible provider default')) {
+      if (out.toLowerCase().includes('initial configuration complete')) {
         loading.value = false
         appSettingsStore.app.kernel.pid = pid
         appSettingsStore.app.kernel.running = true
