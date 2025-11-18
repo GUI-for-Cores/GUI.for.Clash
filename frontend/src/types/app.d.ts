8f30f0afdefe60a4115f04a6ce3624b3dddf474c
Ayideyia <150233177+Ayideyia@users.noreply.github.com>
2025-11-18 17:52:23 +0800

    feat(settings): add auto-restart option for core on config changes

--- frontend/src/types/app.d.ts
+++ frontend/src/types/app.d.ts
@@ -56,6 +56,7 @@ type AppSettings = {
   autoSetSystemProxy: boolean
   proxyBypassList: string
   autoStartKernel: boolean
+  autoRestartKernel: boolean
   userAgent: string
   startupDelay: number
   connections: {
