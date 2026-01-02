06427440a19ab2164d081282b4b769e74209e28a
Ayideyia <150233177+Ayideyia@users.noreply.github.com>
2026-01-02 16:03:55 +0800

    refactor(core-api): centralize REST and WebSocket logic into api layer

--- frontend/src/api/kernel.ts
+++ frontend/src/api/kernel.ts
@@ -1,7 +1,19 @@
-import { useAppSettingsStore, useProfilesStore } from '@/stores'
-import { Request } from '@/utils/request'
+import { Request } from '@/api/request'
+import { WebSockets } from '@/api/websocket'
+import { useProfilesStore } from '@/stores'
+import { setIntervalImmediately } from '@/utils'
 
-import type { CoreApiConfig, CoreApiProxies, CoreApiConnections } from '@/types/kernel'
+import type {
+  CoreApiConfig,
+  CoreApiProxies,
+  CoreApiConnections,
+  CoreApiLogsData,
+  CoreApiMemoryData,
+  CoreApiTrafficData,
+  CoreApiConnectionsData,
+} from '@/types/kernel'
+
+type WsInstance = { setup?: () => void; cleanup?: () => void; timer?: number }
 
 export enum Api {
   Configs = '/configs',
@@ -14,50 +26,126 @@ export enum Api {
   GEO = '/configs/geo',
 }
 
-const setupKernelApi = () => {
-  const appSettingsStore = useAppSettingsStore()
-  const profilesStore = useProfilesStore()
+const setupCoreApi = (protocol: 'http' | 'ws') => {
+  const { currentProfile: profile } = useProfilesStore()
 
-  const profile = profilesStore.getProfileById(appSettingsStore.app.kernel.profile)
-
-  let base = 'http://127.0.0.1:20113'
+  let base = `${protocol}://127.0.0.1:20113`
   let bearer = ''
 
   if (profile) {
     const controller = profile.advancedConfig['external-controller'] || '127.0.0.1:20113'
     const [, port = 20113] = controller.split(':')
     // TODO: tls
-    base = `http://127.0.0.1:${port}`
+    base = `${protocol}://127.0.0.1:${port}`
     bearer = profile.advancedConfig.secret
   }
 
-  request.base = base
-  request.bearer = bearer
+  if (protocol === 'http') {
+    request.base = base
+    request.bearer = bearer
+  } else {
+    websocket.base = base
+    websocket.bearer = bearer
+  }
 }
 
-const request = new Request({ beforeRequest: setupKernelApi, timeout: 60 * 1000 })
+const request = new Request({ beforeRequest: () => setupCoreApi('http'), timeout: 60 * 1000 })
+const websocket = new WebSockets({ beforeConnect: () => setupCoreApi('ws') })
 
+const longLivedWS: WsInstance = { setup: undefined, cleanup: undefined, timer: -1 }
+const shortLivedWS: WsInstance = { setup: undefined, cleanup: undefined, timer: -1 }
+
+const websocketHandlers = {
+  logs: [] as ((data: CoreApiLogsData) => void)[],
+  memory: [] as ((data: CoreApiMemoryData) => void)[],
+  traffic: [] as ((data: CoreApiTrafficData) => void)[],
+  connections: [] as ((data: CoreApiConnectionsData) => void)[],
+} as const
+
+const createCoreWSHandlerRegister = <S extends C[], C>(source: S, events: WsInstance = {}) => {
+  const register = (cb: S[number]) => {
+    source.push(cb)
+    source.length === 1 && events.setup?.()
+    const unregister = () => {
+      const idx = source.indexOf(cb)
+      idx !== -1 && source.splice(idx, 1)
+      source.length === 0 && events.cleanup?.()
+    }
+    return unregister
+  }
+  return register
+}
+
+// restful api
 export const getConfigs = () => request.get<CoreApiConfig>(Api.Configs)
-
 export const setConfigs = (body = {}) => request.patch<null>(Api.Configs, body)
-
 export const getProxies = () => request.get<CoreApiProxies>(Api.Proxies)
-
 export const getConnections = () => request.get<CoreApiConnections>(Api.Connections)
-
 export const deleteConnection = (id: string) => request.delete<null>(Api.Connections + '/' + id)
-
 export const deleteGroupFixed = (group: string) => request.delete(Api.Proxies + '/' + group)
-
 export const useProxy = (group: string, proxy: string) => {
   return request.put<null>(Api.Proxies + '/' + group, { name: proxy })
 }
-
 export const getProxyDelay = (proxy: string, url: string) => {
   return request.get<Record<string, number>>(Api.ProxyDelay.replace('{0}', proxy), {
     url,
     timeout: 5000,
   })
 }
-
 export const updateGEO = () => request.post<{ message: string } | null>(Api.GEO)
+
+// websocket api
+export const onLogs = createCoreWSHandlerRegister(websocketHandlers.logs, shortLivedWS)
+export const onMemory = createCoreWSHandlerRegister(websocketHandlers.memory)
+export const onTraffic = createCoreWSHandlerRegister(websocketHandlers.traffic)
+export const onConnections = createCoreWSHandlerRegister(websocketHandlers.connections)
+export const connectWebsocket = () => {
+  const { connect: connectLongLived, disconnect: disconnectLongLived } = websocket.createWS([
+    {
+      name: Api.Memory,
+      url: Api.Memory,
+      cb: (data) => websocketHandlers.memory.forEach((cb) => cb(data)),
+    },
+    {
+      name: Api.Traffic,
+      url: Api.Traffic,
+      cb: (data) => websocketHandlers.traffic.forEach((cb) => cb(data)),
+    },
+    {
+      name: Api.Connections,
+      url: Api.Connections,
+      cb: (data) => websocketHandlers.connections.forEach((cb) => cb(data)),
+    },
+  ])
+
+  const { connect: connectShortLived, disconnect: disconnectShortLived } = websocket.createWS([
+    {
+      name: Api.Logs,
+      url: Api.Logs,
+      params: { level: 'debug' },
+      cb: (data) => websocketHandlers.logs.forEach((cb) => cb(data)),
+    },
+  ])
+
+  longLivedWS.setup = () => {
+    longLivedWS.timer = setIntervalImmediately(connectLongLived, 30_000)
+  }
+  longLivedWS.cleanup = () => {
+    clearInterval(longLivedWS.timer)
+    disconnectLongLived()
+  }
+
+  shortLivedWS.setup = () => {
+    shortLivedWS.timer = setIntervalImmediately(connectShortLived, 30_000)
+  }
+  shortLivedWS.cleanup = () => {
+    clearInterval(shortLivedWS.timer)
+    disconnectShortLived()
+  }
+
+  longLivedWS.setup()
+}
+export const disconnectWebsocket = () => {
+  longLivedWS.cleanup?.()
+  shortLivedWS.cleanup?.()
+}
